bbb7e354555d186ef7b1f8e87b25759ab8259d02
松下弓華 <231030@st.yoshida-g.ac.jp>
2024-02-14 19:24:21 +0900

    コメント書き直して綺麗にしてます

--- TOMATO/pause.cpp
+++ TOMATO/pause.cpp
@@ -1,7 +1,6 @@
 //=========================================================
-//シューティングゲーム[pause.cpp]
+//tmt[pause.cpp]
 //Author:Yumika Matsushita
-//
 //=========================================================
 
 //=====================================
@@ -16,32 +15,35 @@
 //=====================================
 //マクロ定義
 //=====================================
-#define MAX_POLYGON	(3)	//ポリゴンの最大数
+#define MAX_POLYGON	(3)				//ポリゴンの最大数
 #define POLYGON_WIDTH	(150.0f)	//ポリゴンの横の長さ
 #define POLYGON_HEIGHT	(100.0f)	//ポリゴンの縦の長さ
 #define Brightness_POLYGON	(51)	//ポリゴンの明るさ
-#define POLYGON_COL (255)			//ポリゴンのカラー
-#define MAX_PL	(4)	//プレイヤーの最大数
+#define POLYGON_MAXCOL (255)		//ポリゴンの不透明度(255)
+#define POLYGON_COL (100)			//ポリゴンの不透明度(100)
+#define MAX_PL	(4)					//プレイヤーの最大数
 
 //=====================================
 //グローバル変数
 //=====================================
-LPDIRECT3DTEXTURE9	g_pTexturePauseBG = NULL;	//テクスチャへのポインタ
-LPDIRECT3DVERTEXBUFFER9	g_pVtxBuffPauseBG = NULL;	//頂点バッファへのポインタ
+LPDIRECT3DTEXTURE9	g_pTexturePauseBG = NULL;			//テクスチャへのポインタ
+LPDIRECT3DVERTEXBUFFER9	g_pVtxBuffPauseBG = NULL;		//頂点バッファへのポインタ
 LPDIRECT3DTEXTURE9	g_pTexturePause[MAX_POLYGON] = {};	//テクスチャへのポインタ
-LPDIRECT3DVERTEXBUFFER9	g_pVtxBuffPause = NULL;	//頂点バッファへのポインタ
-Pause g_pause[MAX_POLYGON];
-int nCntPauseCol = 100;
-int g_nCntChoose = 0;
+LPDIRECT3DVERTEXBUFFER9	g_pVtxBuffPause = NULL;			//頂点バッファへのポインタ
+Pause g_pause[MAX_POLYGON];								//Pauseのポリゴンの構造体
+int g_nCntChoose = 0;									//選択されたポリゴンを光らせる為のカウント
 
 //=====================================
 //初期化処理
 //=====================================
 void InitPause(void)
 {
-	int nCntPause = 0;
+//==========================================================================================
+//ここから次の区切りまではポーズ画面の背景のポリゴンの処理
 
-	LPDIRECT3DDEVICE9 pDevice = GetDevice();	//デバイスの取得
+	int nCntPause = 0;									//for文のカウントの初期化
+
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();			//デバイスの取得
 
 	//テクスチャの読み込み
 	D3DXCreateTextureFromFile(pDevice,
@@ -56,7 +58,7 @@ void InitPause(void)
 		&g_pVtxBuffPauseBG,
 		NULL);
 
-	VERTEX_2D* pVtx;	//頂点情報へのポインタ
+	VERTEX_2D* pVtx;									//頂点情報へのポインタ
 
 	//頂点バッファをロックし、頂点情報へのポインタを取得
 	g_pVtxBuffPauseBG->Lock(0, 0, (void**)&pVtx, 0);
@@ -75,10 +77,10 @@ void InitPause(void)
 	pVtx[3].rhw = 1.0f;
 
 	//頂点カラーの設定
-	pVtx[0].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 120);
-	pVtx[1].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 120);
-	pVtx[2].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 120);
-	pVtx[3].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 120);
+	pVtx[0].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, 120);
+	pVtx[1].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, 120);
+	pVtx[2].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, 120);
+	pVtx[3].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, 120);
 
 	//テスチャ座標の設定
 	pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
@@ -89,7 +91,8 @@ void InitPause(void)
 	//頂点バッファをアンロックする
 	g_pVtxBuffPauseBG->Unlock();
 
-	//==========================================================================================
+//==========================================================================================
+//ここから下までは選択出来るポリゴンの処理
 
 	for (nCntPause = 0; nCntPause < MAX_POLYGON; nCntPause++)
 	{
@@ -99,7 +102,6 @@ void InitPause(void)
 		g_pause[nCntPause].bColUse = false;
 	}
 
-	nCntPauseCol = 100;
 	g_nCntChoose = 0;
 
 	//テクスチャの読み込み
@@ -142,10 +144,10 @@ void InitPause(void)
 		pVtx[3].rhw = 1.0f;
 
 		//頂点カラーの設定
-		pVtx[0].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 100);
-		pVtx[1].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 100);
-		pVtx[2].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 100);
-		pVtx[3].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 100);
+		pVtx[0].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_COL);
+		pVtx[1].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_COL);
+		pVtx[2].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_COL);
+		pVtx[3].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_COL);
 
 		//テスチャ座標の設定
 		pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
@@ -156,9 +158,10 @@ void InitPause(void)
 		pVtx += 4;
 	}
 
+	//配置しているとこ
 	for (nCntPause = 0; nCntPause < MAX_POLYGON; nCntPause++)
 	{
-		SetPause(D3DXVECTOR3((SCREEN_WIDTH / 4), (nCntPause * 200.0f) + 100.0f, 0.0f));
+		SetPause(D3DXVECTOR3((SCREEN_WIDTH / 4), (nCntPause * 200.0f) + 250.0f, 0.0f));
 	}
 
 	//頂点バッファをアンロックする
@@ -170,6 +173,9 @@ void InitPause(void)
 //=====================================
 void UninitPause(void)
 {
+//==================================================================================
+//区切られているところまで背景のポリゴンの処理 
+	
 	//テクスチャの破棄
 	if (g_pTexturePauseBG != NULL)
 	{
@@ -184,7 +190,8 @@ void UninitPause(void)
 		g_pVtxBuffPauseBG = NULL;
 	}
 
-	//==================================================================================
+//==================================================================================
+//下まで背景のポリゴンの処理 
 
 	for (int nCntPause = 0; nCntPause < MAX_POLYGON; nCntPause++)
 	{
@@ -209,77 +216,99 @@ void UninitPause(void)
 //=====================================
 void UpdatePause(void)
 {
-	VERTEX_2D* pVtx;	//頂点情報へのポインタ
+	VERTEX_2D* pVtx;		//頂点情報へのポインタ
 
 	//頂点バッファをロックし、頂点情報へのポインタを取得
 	g_pVtxBuffPause->Lock(0, 0, (void**)&pVtx, 0);
 
+	//構造体のフラグを全てfalseにする
 	for (int nCntPause = 0; nCntPause < MAX_POLYGON; nCntPause++)
 	{
 		g_pause[nCntPause].bColUse = false;
 	}
 
+	//↑キー押されたら
 	if (GetKeyboardTrigger(DIK_UP) == true || GetJoypadTrigger(JOYKEY_UP,0) == true)
 	{
-		g_nCntChoose--;
+		g_nCntChoose--;		//カウントをマイナスする(上に進む)
 	}
+
+	//↓キー押されたら
 	if (GetKeyboardTrigger(DIK_DOWN) == true || GetJoypadTrigger(JOYKEY_DOWN,0) == true)
 	{
-		g_nCntChoose++;
+		g_nCntChoose++;		//カウントをプラスする(下に進む)
 	}
+
+	//もしカウントが使用されているポリゴン数よりもマイナス値になってしまった時
 	if (g_nCntChoose <= -1)
 	{
-		g_nCntChoose = 2;
+		g_nCntChoose = 2;	//強制的にカウントを2にする(retryからendにとばす)
 	}
+
+	//もしカウントが使用されているポリゴン数よりもプラス値になってしまった時
 	if (g_nCntChoose >= 3)
 	{
-		g_nCntChoose = 0;
+		g_nCntChoose = 0;	//強制的にカウントを0にする(endからretryにとばす)
 	}
 
+	//選ばれたカウントの値のフラグをtrueにする
 	g_pause[g_nCntChoose].bColUse = true;
 
+	//ポリゴンの数分回してます
 	for (int nCntPause = 0; nCntPause < MAX_POLYGON; nCntPause++)
 	{
+		//もし選ばれたポリゴンのフラグがfalseの場合
 		if (g_pause[nCntPause].bColUse == false)
-		{
+		{//不透明度を低くする
 			//頂点カラーの設定(false時)
-			pVtx[0].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 100);
-			pVtx[1].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 100);
-			pVtx[2].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 100);
-			pVtx[3].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, 100);
-		}
-		if (g_pause[nCntPause].bColUse == true)
-		{
-			//頂点カラーの設定(true時)
-			pVtx[0].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, POLYGON_COL);
-			pVtx[1].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, POLYGON_COL);
-			pVtx[2].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, POLYGON_COL);
-			pVtx[3].col = D3DCOLOR_RGBA(POLYGON_COL, POLYGON_COL, POLYGON_COL, POLYGON_COL);
+			pVtx[0].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_COL);
+			pVtx[1].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_COL);
+			pVtx[2].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_COL);
+			pVtx[3].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_COL);
 		}
 
-		pVtx += 4;
+		//もし選ばれたポリゴンのフラグがtrueの場合
+		if (g_pause[nCntPause].bColUse == true)
+		{//不透明度を高くする
+			//頂点カラーの設定(true時)
+			pVtx[0].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL);
+			pVtx[1].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL);
+			pVtx[2].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL);
+			pVtx[3].col = D3DCOLOR_RGBA(POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL, POLYGON_MAXCOL);
+		}
+
+		pVtx += 4;			//いつもの
 	}
 
+	//プレイヤーの数ぶん回してます
 	for (int nCntPause = 0; nCntPause < MAX_PL; nCntPause++)
 	{
+		//もしポリゴンの0番が選ばれた場合
 		if (g_pause[0].bColUse == true)
 		{
+			//上記を踏まえたうえでもしAボタンを押した場合
 			if (GetJoypadTrigger(JOYKEY_A, nCntPause) == true)
 			{
 				// ゲーム画面に戻る処理してください
 				//SetFade(MODE_GAME);
 			}
 		}
+
+		//もしポリゴンの1番が選ばれた場合
 		if (g_pause[1].bColUse == true)
 		{
+			//上記を踏まえたうえでもしAボタンを押した場合
 			if (GetJoypadTrigger(JOYKEY_A, nCntPause) == true)
 			{
 				// チュートリアル画面に戻る処理してください
 				//SetFade(MODE_TUTORIAL);
 			}
 		}
+
+		//もしポリゴンの2番が選ばれた場合
 		if (g_pause[2].bColUse == true)
 		{
+			//上記を踏まえたうえでもしAボタンを押した場合
 			if (GetJoypadTrigger(JOYKEY_A, nCntPause) == true)
 			{
 				// タイトル画面に戻る処理してください
